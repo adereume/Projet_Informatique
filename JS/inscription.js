20939bf43a0849bd810d50812967f68cacacfcf0
adereume <dereumetz.anais@gmaiL.com>
2017-02-01 16:27:17 +0100

    Inscription & Connection V1

--- JS/inscription.js
+++ JS/inscription.js
@@ -1,9 +1,4 @@
 $(document).on("click", "input[type=submit]", function() {
-	$("#error").css("display", "none");
-	$('input[type=text]').attr("class", null);
-	$('input[type=password]').attr("class", null);
-	$("p#text-error").remove();
-
 	var error = false;
 
 	if($("input[name=firstname]").val() == "") {
@@ -12,18 +7,15 @@ $(document).on("click", "input[type=submit]", function() {
 			$('input[name=firstname]').attr("class", "input-error");
 		}		
 		error = true;
-	} else if($('input[name=firstname]').attr("class") == "input-error") 
-		$('input[name=firstname]').attr("class", null);
-	
-	
+	}
+
 	if($("input[name=lastname]").val() == "") {
 		if($('input[name=lastname]').attr("class") != "input-error") {
 			$('input[name=lastname]').after("<p id='text-error'>Ce champs est obligatoire</p>");
 			$('input[name=lastname]').attr("class", "input-error");
 		}		
 		error = true;
-	} else if($('input[name=lastname]').attr("class") == "input-error") 
-		$('input[name=lastname]').attr("class", null);
+	} 
 
 	if($("input[name=password]").val() == "") {
 		if($('input[name=password]').attr("class") != "input-error") {
@@ -31,8 +23,7 @@ $(document).on("click", "input[type=submit]", function() {
 			$('input[name=password]').attr("class", "input-error");
 		}
 		error = true;
-	} else if($('input[name=password]').attr("class") == "input-error") 
-		$('input[name=password]').attr("class", null);
+	} 
 
 	if(! error) {
 		$.ajax({
@@ -40,10 +31,11 @@ $(document).on("click", "input[type=submit]", function() {
 	       	url: 'http://localhost/projet_mobile/data.php', 
 	       	type: 'GET',
 	       	data: {
-	       		action: "connexionWeb",
+	       		action: "inscription",
 	       		firstname: $("input[name=firstname]").val(),       		
 	       		lastname: $("input[name=lastname]").val(),
-	       		password: $("input[name=password]").val()
+	       		password: $("input[name=password]").val(),
+	       		type: "TEACHER"
 	       	},
 	       	success: function(oRep) {
 	       		console.log("Success :");
