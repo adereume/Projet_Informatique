faa4bbbe25a4f0fb2271aac0ac9d82154ff44e0c
clem6237 <clementruffin62@gmail.com>
2017-02-22 21:42:58 +0100

    Page Seance
    Ajout de l’oeil sur les éléments
    Requêtes périodiques
    Reste -> requête en base au clic sur l’oeil + CSS

--- JS/seance.js
+++ JS/seance.js
@@ -1,6 +1,8 @@
 var idUser;
 var idSeance;
 
+var interval
+
 $(document).ready(function() {
 
     var parameters = location.search.substring(1).split("&");
@@ -16,31 +18,49 @@ $(document).ready(function() {
 });
 
 $(document).on("click", "div[class=task]", function() {
-    $("#title").text("");
-    $("#description").text("");
+    var idTask = $(this).attr("value");
 
-    displayTask($(this).attr("value"));
+    clearInterval(interval);
+    displayTask(idTask);
+    
+    interval = setInterval(function() { displayTask(idTask); }, 10000);
 });
 
 $(document).on("click", "div[class=question]", function() {
-    $("#title").text("");
-    $("#description").text("");
+    var idQuestion = $(this).attr("value");
 
-    displayQuestion($(this).attr("value"));
+    clearInterval(interval);
+    displayQuestion(idQuestion);
+
+    interval = setInterval(function() { displayQuestion(idQuestion); }, 10000);
 });
 
 $(document).on("click", "div[class=homework]", function() {
-    $("#title").text("");
-    $("#description").text("");
+    var idHomework = $(this).attr("value");
 
-    displayHomework($(this).attr("value"));
+    clearInterval(interval);
+    displayHomework(idHomework);
+
+    interval = setInterval(function() { displayHomework(idHomework); }, 10000);
 });
 
 $(document).on("click", "div[class=note]", function() {
-    $("#title").text("");
-    $("#description").text("");
+    var idNote = $(this).attr("value");
 
-    //displayNote($(this).attr("value"));
+    clearInterval(interval);
+    //displayNote(idNote);
+
+    //interval = setInterval(function() { displayNote(idNote); }, 10000);
+});
+
+$(document).on("click", "img[class=eye-active]", function() {
+    $(this).removeClass("eye-active");
+    $(this).addClass("eye-inactive");
+});
+
+$(document).on("click", "img[class=eye-inactive]", function() {
+    $(this).removeClass("eye-inactive");
+    $(this).addClass("eye-active");
 });
 
 function getSeance() {
@@ -62,11 +82,11 @@ function getSeance() {
                 for (var i = 0; i < seance.length; i++) {
                     switch (seance[i].type) {
                         case "Tache" :
-                            $("#tasks").append("<div class='task' value='" + seance[i].id + "'>" + seance[i].titre + "</div>");
+                            $("#tasks").append("<div class='task' value='" + seance[i].id + "'>" + seance[i].titre + "<img class='eye-active'/></div>");
                             break;
 
                         case "Question" :
-                            $("#tasks").append("<div class='question' value='" + seance[i].id + "'>" + seance[i].titre + "</div>");
+                            $("#tasks").append("<div class='question' value='" + seance[i].id + "'>" + seance[i].titre + "<img class='eye-active'/></div>");
                             break;
                     }
                 }
@@ -74,13 +94,13 @@ function getSeance() {
                 var homework = oRep.homework;
 
                 for (var i = 0; i < homework.length; i++) {
-                    $("#homeworks").append("<div class='homework' value='" + homework[i].id + "'>" + homework[i].titre + "</div>");
+                    $("#homeworks").append("<div class='homework' value='" + homework[i].id + "'>" + homework[i].titre + "<img class='eye-active'/></div>");
                 }
 
                 var note = oRep.note;
 
                 for (var i = 0; i < note.length; i++) {
-                    $("#notes").append("<div class='note' value='" + note[i].id + "'>" + note[i].description + "</div>");
+                    $("#notes").append("<div class='note' value='" + note[i].id + "'>" + note[i].description + "<img class='eye-active'/></div>");
                 }
             } else {
                 window.location = "../index.html";
@@ -104,11 +124,21 @@ function displayTask(idTask) {
             idTache: idTask
         },
         success: function(oRep) {
-            console.log(oRep.tache);
+            console.log(oRep);
             if(oRep.tache != null) {
 
                 $("#title").text("Tâche - " + oRep.tache[0].titre);
                 $("#description").text(oRep.tache[0].description);
+                $("#contenu").text("");
+
+                // ça ça doit disparaître :)
+                oRep.question = [ {question: "quoi ?", answer: "euh"}, {question: "où ?", answer: "ici"}];
+                
+                if (oRep.question != null) {
+                    for (var i = 0; i < oRep.question.length; i++) {
+                        $("#contenu").append("<br/> Q - " + oRep.question[i].question + "<br/> R - " + oRep.question[i].answer + "<br/>");
+                    }
+                }
 
             } else {
                 window.location = "../index.html";
@@ -136,6 +166,17 @@ function displayQuestion(idQuestion) {
             if(oRep.question != null) {
 
                 $("#title").text("Question - " + oRep.question[0].description);
+                $("#description").text("");
+                $("#contenu").text("");
+
+                // ça ça doit disparaître :)
+                oRep.reponses = [ {answer: "euh"}, {answer: "ici"}];
+                
+                if (oRep.reponses != null) {
+                    for (var i = 0; i < oRep.reponses.length; i++) {
+                        $("#contenu").append("<br/> R - " + oRep.reponses[i].answer);
+                    }
+                }
 
             } else {
                 window.location = "../index.html";
@@ -165,6 +206,9 @@ function displayHomework(idHomework) {
 
                 $("#title").text("Devoir - " + oRep.retour[0].titre);
                 $("#description").text(oRep.retour[0].description);
+                $("#contenu").text("");
+
+                $("#contenu").append("Echéance : " + oRep.retour[0].dueDate);
 
             } else {
                 //window.location = "../index.html";
